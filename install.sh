3a39feb3d75dd7fcc3d0f24fdff063b6fe92504d
Amitai Burstein <amitai@gizra.com>
2013-05-15 23:20:38 +0300

    Add composer manager.

--- install.sh
+++ install.sh
@@ -8,5 +8,10 @@ cd www
 mkdir sites/default/files
 chmod -R 777 sites/default/files
 
+# todo: Move this to a better place
+drush dl composer-8.x-1.0-alpha10 -n
+drush vset composer_manager_vendor_dir profiles/dekyll/libraries/composer
+drush composer-manager install
+
 drush si -y dekyll --account-pass=admin --db-url=mysql://root:root@localhost/dekyll
 drush mi --all --user=1
