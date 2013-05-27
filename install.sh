69a9ddade72f93540fa572f05171b86be3d1189c
Amitaibu <amitai@gizra.com>
2013-05-27 15:59:28 +0300

    Fix composer download.

--- install.sh
+++ install.sh
@@ -3,14 +3,14 @@
 chmod 777 www/sites/default/
 bash scripts/build
 
+# Get composer
+curl -sS https://getcomposer.org/installer | php
+php composer.phar install --working-dir="./dekyll/libraries"
+
 cd www
 
 mkdir sites/default/files
 chmod -R 777 sites/default/files
-
-# Get composer
-curl -sS https://getcomposer.org/installer | php
-php composer.phar install --working-dir="./libraries"
 
 drush si -y dekyll --account-pass=admin --db-url=mysql://root:root@localhost/dekyll
 drush vset dekyll_git_repository 'git@github.com:amitaibu/jekyll-bootstrap.git'
