91a03596ab8b1d7e2342ac115aab358ecd6f7a5a
Amitaibu <amitai@gizra.com>
2013-05-29 15:00:26 +0300

    Remove install.sh.

--- default.install.sh
+++ default.install.sh
@@ -1,11 +1,15 @@
 #!/bin/bash
 
-chmod 777 www/sites/default
-rm -rf www/
-mkdir www
-
+chmod 777 www/sites/default/
 bash scripts/build
+
+# Get composer
+curl -sS https://getcomposer.org/installer | php
+php composer.phar install --working-dir="./dekyll/libraries"
+
 cd www
 
-drush si -y dekyll --account-pass=admin --db-url=mysql://root@localhost/dekyll
-drush mi --all --user=1
+mkdir sites/default/files
+chmod -R 777 sites/default/files
+
+drush si -y dekyll --account-pass=admin --db-url=mysql://root:root@localhost/dekyll
