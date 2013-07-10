a8e6823ed2abb2547d6ba549cba51917bad71883
Amitai Burstein <amitai@gizra.com>
2013-07-10 21:46:06 +0300

    IF raw value is empty, unset it from the YAML.

--- dekyll/modules/custom/dekyll_sync/plugins/content_sync/raw/ContentSyncRaw.class.php
+++ dekyll/modules/custom/dekyll_sync/plugins/content_sync/raw/ContentSyncRaw.class.php
@@ -27,17 +27,13 @@ class ContentSyncRaw extends ContentSyncBase {
   public function export(EntityDrupalWrapper $wrapper, &$yaml = array(), &$text = '', $files_info) {
     $plugin_name = $this->plugin['name'];
     foreach ($this->syncMap[$plugin_name] as $field_name) {
-      if (!$value = $wrapper->{$field_name}->raw()) {
-        continue;
-      }
-
-      if ($value == 'nil') {
-        // @todo: Make configurable?
-        return;
-      }
-
       $instance = field_info_instance($wrapper->type(), $field_name, $wrapper->getBundle());
       $jekyll_name = $this->getJekyllName($instance, $field_name);
+
+      if (!($value = $wrapper->{$field_name}->raw()) || $value == 'nil') {
+        unset($yaml[$jekyll_name]);
+        continue;
+      }
 
       $yaml[$jekyll_name] = $value;
     }
