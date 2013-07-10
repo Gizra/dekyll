a8916b0d854340b2e612f9f8a20e4330f22405ff
Amitaibu <amitai@gizra.com>
2013-07-10 08:35:43 +0300

    Improve comment on bash.

--- default.install.sh
+++ default.install.sh
@@ -18,9 +18,14 @@ drush si -y dekyll --account-pass=admin --db-url=mysql://root:root@localhost/dek
 # Set the SSH key
 drush vset dekyll_ssh_key "$(cat ~/.ssh/id_rsa.pub)"
 
-# Set Github Application
-# drush vset github_connect_client_id YOUR-CLIENT-ID
-# drush vset github_connect_client_secret YOUR-CLIENT-SECRET
+# Set Github Application, the following setting is Using a development app
+# that assumes your site is located on http://localhost/dekyll/www
+# If you would like to deploy the site on another address, you should create
+# a new app in https://github.com/settings/applications, and replace the client
+# ID and client secret values.
+drush vset github_connect_client_id "6b8801a904e18533efaa"
+drush vset github_connect_client_secret "e667b19b21e4e6a60a8d1f656d41a5b81da5d4de"
+
 
 # Execute Dekyll queue workers.
 . ../dekyll.sh
