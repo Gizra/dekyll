efb6c056e3759583791a997b106a5a4e1b09ef4c
Amitaibu <amitai@gizra.com>
2013-07-11 15:44:25 +0300

    Add URI.

--- dekyll.sh
+++ dekyll.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-drush process-waiting-queue dekyll_clone -v &
-drush process-waiting-queue dekyll_export -v &
+drush process-waiting-queue dekyll_clone -v --uri=http://localhost/dekyll/www &
+drush process-waiting-queue dekyll_export -v --uri=http://localhost/dekyll/www &
